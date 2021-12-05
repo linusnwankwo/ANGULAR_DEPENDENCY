01f146387d1b1db58881729499154438f126dd31
Linus Nwankwo <nwankwolinus9@gmail.com>
2021-12-05 23:17:46 +0100

    created an emp-info component

--- dependency/src/app/app.module.ts
+++ dependency/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpInfoComponent } from './emp-info/emp-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpInfoComponent
   ],
   imports: [
     BrowserModule,
