815f9bfd304795fbc678fb90453d0a9d8b8d7cd7
Linus Nwankwo <nwankwolinus9@gmail.com>
2021-12-05 23:10:36 +0100

    Revert "created empt-info files"
    This reverts commit 88a7b55501a75daf0e24c5d4cff22bdaa2b628ee.

--- dependency/src/app/app.module.ts
+++ dependency/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmpInfoComponent } from './emp-info/emp-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmpInfoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
